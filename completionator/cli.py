68f1f63b3a716daecb8d7f1364218a58efe2f6bd
Steve Johnson <steve@graffitilabs.com>
2017-03-24 18:43:42 -0700

    Fancy pants completionator stuff

---
+++ completionator/cli.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+import csv
+import json
+import sys
+from collections import namedtuple
+from os import makedirs
+from pathlib import Path
+from random import choice
+
+import appdirs
+import click
+import requests
+
+from .html2csv import html2csv
+
+
+CSV_PATH = Path(appdirs.user_data_dir('completionator', 'steveasleep')) / 'games.csv'
+SETTINGS_PATH = Path(appdirs.user_data_dir('completionator', 'steveasleep')) / 'settings.json'
+makedirs(str(CSV_PATH.parent), exist_ok=True)
+
+
+def get_settings(change_user=False):
+    if change_user or not SETTINGS_PATH.exists():
+        user_id = click.prompt("What's your user ID?", type=int)
+        settings = {'user_id': user_id}
+        with SETTINGS_PATH.open('w') as f:
+            json.dump(settings, f)
+        return settings
+    else:
+        with SETTINGS_PATH.open('r') as f:
+            return json.load(f)
+
+
+def update_csv(user_id):
+    r = requests.get(
+        'http://completionator.com/Collection/ExportToExcel/{}?keyword=&isHidden=false&shouldBreakOutCompilationGames=true&sortColumn=GameName&sortDirection=ASC'.format(user_id))
+    with CSV_PATH.open('w') as f:
+        html2csv(r.text, f)
+
+
+def get_games():
+    with open('games.csv', 'r') as f:
+        reader = csv.reader(f)
+        headings = next(reader)
+        fields = [
+            field.lower().replace(' ', '_')
+            for field in headings
+        ]
+        Game = namedtuple('Game', fields)
+        return headings, [Game(*columns) for columns in reader]
+
+
+@click.command()
+@click.option('--change-user', default=False, is_flag=True)
+@click.option('--update', default=False, is_flag=True)
+@click.option('--active', default=False, is_flag=True)
+@click.option('--todo', default=False, is_flag=True)
+@click.option('--fmt', default='name', type=click.Choice(['name', 'repr', 'csv']))
+@click.option('--random/--no-random', default=False)
+def cli(change_user, update, active, todo, fmt, random):
+    settings = get_settings(change_user=change_user)
+
+    if update or not CSV_PATH.exists():
+        update_csv(user_id=settings['user_id'])
+
+    headings, games = get_games()
+    filtered_games = [
+        g for g in games if (
+            active and g.now_playing == 'Yes' or
+            todo and g.progress_status == 'Incomplete'
+        )
+    ]
+    if random:
+        filtered_games = [choice(filtered_games)]
+
+    csv_writer = None
+    if fmt == 'csv':
+        csv_writer = csv.writer(sys.stdout)
+        csv_writer.writerow(headings)
+
+    for game in filtered_games:
+        if fmt == 'name':
+            click.echo(game.name)
+        elif fmt == 'repr':
+            click.echo(repr(game))
+        elif fmt == 'csv':
+            csv_writer.writerow(game)
+
+
+if __name__ == '__main__':
+    cli()
--- completionator/html2csv.py
+++ completionator/html2csv.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 import csv
-import sys
 from html.parser import HTMLParser
 
 
-writer = csv.writer(sys.stdout)
-
-
 class CompletionatorHTMLParser(HTMLParser):
-    def __init__(self, *args, **kwargs):
+    """Interpret Completionator's silly "excel" format (i.e. HTML table)"""
+
+    def __init__(self, writer, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.writer = writer
         self._current_row = []
         self._current_row_i = 0
 
@@ -21,12 +20,14 @@ class CompletionatorHTMLParser(HTMLParser):
 
     def handle_endtag(self, tag):
         if tag == 'tr':
-            writer.writerow(self._current_row[:6])
+            # remaining columns tend to be empty
+            self.writer.writerow(self._current_row[:6])
 
     def handle_data(self, data):
         self._current_row[len(self._current_row) - 1] = data
 
 
-parser = CompletionatorHTMLParser()
-with open(sys.argv[1], 'r') as f:
-    parser.feed(f.read())
+def html2csv(s, out_file):
+    writer = csv.writer(out_file)
+    parser = CompletionatorHTMLParser(writer)
+    parser.feed(s)
